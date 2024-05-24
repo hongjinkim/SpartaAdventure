c9427b123c0938854f3539113e22dda7a563d94c
ghdwl <ghdwls4654@naver.com>
2024-05-24 20:13:42 +0900

    Player, PlayerCondition, UI 스크립트 추가

---
+++ SpartaAdventure/Assets/Scripts/Singleton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    instance = singletonObject.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(instance);
+    }
+}
